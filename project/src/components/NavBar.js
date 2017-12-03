125cd2576d495077faf39cdd9313372414098f48
Joseph Zhang <josephzhang@D-69-91-136-182.dhcp4.washington.edu>
2017-12-03 15:47:04 -0800

    initial make of navbar

--- project/src/components/NavBar.js
+++ project/src/components/NavBar.js
@@ -1,10 +1,26 @@
+
 import React from 'react';
 
 export default class NavBar extends React.Component {
     render() {
         return (
             <div>
-                <h1>navbar</h1>
+                <nav className="navbar navbar-expand-md bg-dark">
+                    <a class="navbar-brand" href="#">
+                        <img src="http://www.mataro-parc.com/sites/default/files/field/operador-logo/udon_-_logo.jpg" alt="udon-logo"/>
+                    </a>
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="http://freshudon.com/menu/">Menu</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="http://facebook.com/fresh.noodle.station/">Facebook</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="http://twitter.com">Twitter</a>
+                        </li>
+                    </ul>
+                </nav>
             </div>
         );
     }
