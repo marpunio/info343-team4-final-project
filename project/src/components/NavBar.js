4ebc8fa5ce3c52f750ad4cf130e5d741df886701
Joseph Zhang <josephzhang@D-173-250-143-176.dhcp4.washington.edu>
2017-12-06 11:48:45 -0800

    fixing merge problems

--- project/src/components/NavBar.js
+++ project/src/components/NavBar.js
@@ -25,7 +25,6 @@ export default class NavBar extends React.Component {
                     <a className="navbar-brand" href={constants.routes.home}>
                         <img src="http://www.mataro-parc.com/sites/default/files/field/operador-logo/udon_-_logo.jpg" alt="udon-logo" />
                     </a>
-<<<<<<< HEAD
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item mx-2">
                             <a class="nav-link" href={constants.routes.menu}>Menu</a>
@@ -38,20 +37,6 @@ export default class NavBar extends React.Component {
                         </li>
                         <li class="nav-item mx-2">
                             <a class="nav-link" href={constants.routes.signup}>Sign Up</a>
-=======
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href={constants.routes.menu}>Menu</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="https://www.facebook.com/udon.noodle.station/">Facebook</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="http://twitter.com">Twitter</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href={constants.routes.contact}>Contact Us</a>
->>>>>>> 637b8bc51da1ea57769636887e9f87e9cebf5560
                         </li>
                         {
                             this.props.user ?
