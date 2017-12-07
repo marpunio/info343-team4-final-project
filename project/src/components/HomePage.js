00ae19ffcfc10f147343e278a2112908657a6b54
Joseph Zhang <josephzhang@D-173-250-148-49.dhcp4.washington.edu>
2017-12-06 21:04:52 -0800

    homepage tweaks

--- project/src/components/HomePage.js
+++ project/src/components/HomePage.js
@@ -10,26 +10,28 @@ export default class HomePage extends React.Component {
                     <img id="logo-bowl" src="https://i.imgur.com/bL3Puos.png"/>
                     <img src="https://udonseattle.files.wordpress.com/2011/12/logo-bowl.png"/>
                 </div>
-                <div className="text-center">
+                <div id="steps" className="text-center">
                     <h2 >It's as easy as <span class="udon-red">1, 2, 3!</span></h2>
                 </div>
-                <div className="d-flex flex-nowrap text-center my-2">
-                    <div className="mx-4">
-                        <p>Step 1: Choose your favorite <span class="udon-red">NOODLE!</span></p>
+                <div id="steps" className="d-flex flex-nowrap justify-content-center my-2">
+                    <div className="steps mx-4">
+                        <p>1. <span class="udon-red">NOODLE!</span></p>
                         <img src="https://udonseattle.files.wordpress.com/2011/12/menu-kitsune.png?w=170"/>
                         <p><i>Kitsune Udon</i></p>
                     </div>
-                    <div className="mx-4">
-                        <p>Step 2: Add your favorite <span class="udon-red">TOPPINGS & SIDES!</span></p>
+                    <div className="steps mx-4">
+                        <p>2. <span class="udon-red">TOPPINGS & SIDES!</span></p>
                         <img src="https://udonseattle.files.wordpress.com/2011/12/menu-karaage.png?w=170"/>
                         <p><i>Karaage</i></p>
                     </div>
-                    <div className="mx-4">
-                        <p>Step 3: Add a <span className="udon-red">DRINK or DESSERT!</span></p>
+                    <div className="steps mx-4">
+                        <p>3. <span className="udon-red">DRINK or DESSERT!</span></p>
                         <img src="https://udonseattle.files.wordpress.com/2011/12/menu-specialty-drink.png?w=170"/>
                         <p><i>Specialty Drink</i></p>
                     </div>
                 </div>
+                <hr/>
+                <p><strong>U<span class="udon-red">:D</span>on Fresh Japanese Noodle Station</strong> is a pair of Seattle restaurants specializing in serving fresh, house-made udon noodles cooked to order and delivered in a fast, entertaining, and interactive fashion. Our guests can watch the udon noodles being made, as they customize their meal with freshly prepared toppings and sides to their preference.</p>
             </div>
         );
     }
