16ca8def915ddbb95d6cc7eaf66afb5bc0a016f4
Alexander Zhou <alexanderzhou@D-173-250-202-99.dhcp4.washington.edu>
2017-12-06 23:52:21 -0800

    added half-way finished NavBar and css

--- project/src/components/NavBar.js
+++ project/src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
@@ -6,9 +7,35 @@ import constants from './constants';
 import '../css/NavBar.css';
 
 export default class NavBar extends React.Component {
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-expand-md bg-dark">
+                    <a className="navbar-brand" href={constants.routes.home}>
+                        <img src="http://www.mataro-parc.com/sites/default/files/field/operador-logo/udon_-_logo.jpg" alt="udon-logo" />
+                    </a>
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item mx-2">
+                            <a className="nav-link" href={constants.routes.menu}>Menu</a>
+                        </li>
+                        <li className="nav-item mx-2">
+                            <a className="nav-link" href={constants.routes.contact}>Contact Us</a>
+                        </li>
+                        {
+                            this.props.user ?
+                                <Dropdown /> :
+                                <SignRedirect />
+                        }
+                    </ul>
+                </nav>
+            </div>
+        );
+    }
+}
+
+class Dropdown extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
         this.handleSignOut = this.handleSignOut.bind(this);
     }
 
@@ -21,35 +48,30 @@ export default class NavBar extends React.Component {
     render() {
         return (
             <div>
-                <nav className="navbar navbar-expand-md">
-                    <a className="navbar-brand" href={constants.routes.home}>
-                        <img src="http://www.mataro-parc.com/sites/default/files/field/operador-logo/udon_-_logo.jpg" alt="udon-logo" />
-                    </a>
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item mx-2">
-                            <a className="barlow nav-link" href={constants.routes.menu}>Menu</a>
-                        </li>
-                        <li className="nav-item mx-2">
-                            <a className="barlow nav-link" href={constants.routes.contact}>Contact Us</a>
-                        </li>
-                        {/* checks if an account is logged in */}
-                        {
-                            this.props.user ?
-                                <li className="nav-item">
-                                    <button className="btn btn-primary" onClick={this.handleSignOut} type="submit">Sign out</button>
-                                    <p>{this.props.privilege}</p>
-                                </li> :
-                                <div className="d-flex flex-nowrap">
-                                    <li className="nav-item">
-                                        <a className="barlow nav-link" href={constants.routes.signin}>Sign In</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="barlow nav-link" href={constants.routes.signup}>Sign Up</a>
-                                    </li>
-                                </div>
-                        }
-                    </ul>
-                </nav>
+                <ul>
+                    <li><a href={constants.routes.home}>Profile</a></li>
+                    <li><a href={constants.routes.settings}>Settings</a></li>
+                    <li>
+                        <form onSubmit={this.handleSignOut}>
+                            <button type="submit">Sign Out</button>
+                        </form>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+class SignRedirect extends React.Component {
+    render() {
+        return (
+            <div className="d-flex flex-nowrap">
+                <li className="nav-item">
+                    <a className="nav-link" href={constants.routes.signin}>Sign In</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href={constants.routes.signup}>Sign Up</a>
+                </li>
             </div>
         );
     }
